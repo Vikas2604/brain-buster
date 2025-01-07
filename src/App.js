acc905b5fab22de33da95ceb4485540c9bd4b398
Vikas Amarnath Pandey <89534815+Vikas2604@users.noreply.github.com>
2025-01-07 13:22:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './components/NavBar';
+import Page2 from './components/Page2';
+import Hero from './components/Hero';
+import Potential from './components/Potential';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Hero />
+      <Page2 />
+      <Potential />
     </div>
   );
 }
